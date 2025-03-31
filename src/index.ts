13480403b6f09ca589f89ac1428a2a8504bff396
Tejas Nikhar <tejastn10@gmail.com>
2025-03-31 08:32:00 +0530

    feat: create index file to export core modules for AWS Lambda utilities

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./info";
+export * from "./logging";
+export * from "./performance";
+export * from "./config";
+export * from "./middleware";
+export * from "./decorators";
